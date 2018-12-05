a8870094451d4578a05c355d03db76a71a626c77
julianna220 <45428572+julianna220@users.noreply.github.com>
2018-12-05 10:25:49 +0100

    Add files via upload
    klasa GamePanel, klasa GameStatus, klasa GameConst

---
+++ GameWindow.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package WJP_GraEdukacyjna;
+
+import javax.swing.JFrame;
+import java.awt.GridLayout;
+import java.awt.Toolkit;
+import java.awt.Cursor;
+import java.awt.Point;
+
+/**
+ *
+ * @author Lucek
+ */
+public class GameWindow extends JFrame{
+    
+    public GameWindow (int width, int height, int x, int y){
+        
+        super();
+        setSize(width, height); //ustawienie wymiarów okna
+        setLocation(x,y); //ustawienie pozycji okna
+        setResizable(false); //zablokowanie opcji zmiany rozmiaru okna
+        setUndecorated(true); //ukrycie ramki i przycisków kontrolnych
+        initGUI(width,height); // wywołanie metody budowy interfejsu
+        setVisible(true); //wyświetlenie okna
+        animationLoop(); //uruchomienie pętli animacji gry
+         
+    }
+    
+    
+    private void initGUI(int width, int height){
+        setLayout(new GridLayout(1,1)); // rozkład 
+        GameConst.loadInitialImages(); //
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        Cursor tCursor = tk.createCustomCursor(GameConst.cursorImage, new Point(10,10), "Target Cursor");
+        setCursor(tCursor);
+        add(new GamePanel(width,height)); //dodanie nowego panelu gry zawierającego akcje
+    }
+    
+    
+    private void animationLoop() {
+        GameConst.startTime = System.currentTimeMillis(); //pobranie liczby milisekund
+        long currTime = GameConst.startTime;
+    
+        while (currTime - GameConst.startTime < GameConst.GAME_TIME) {
+          long elapsedTime = System.currentTimeMillis() - currTime;
+          //liczenie czasu gry - mĹĽe siÄ™ przydaÄ‡ w ograniczeniach czasowycho 
+          //w tej demonstracji nie wykorzystane
+          currTime += elapsedTime;
+          
+          //odrysuj kolejny ekran gry (nowe pozycje obiektĂłw - symulacja ruchu)
+          repaint();
+          
+          // przerwa w czasie
+          try {
+            Thread.sleep(80);
+          } catch (InterruptedException ex) {System.out.println("WyjÄ…tek: "+ex);      }
+        }//koniec while
+    }//koniec animationLoop()
+    }
---
+++ WJP_GraEdukacyjna.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package WJP_GraEdukacyjna;
+import java.awt.Toolkit;
+
+/**
+ *
+ * @author Lucek
+ */
+public class WJP_GraEdukacyjna {
+
+
+    public static void main(String[] args) {
+       
+        //Ustalenie rozmiaru gry
+        int gameWidth = 1024;
+        int gameHeight = 768;
+        
+        //Pobieranie rozmiaru gry
+        int screenWidth = Toolkit.getDefaultToolkit().getScreenSize().width;
+        int screenHeight = Toolkit.getDefaultToolkit().getScreenSize().height;
+        
+        //obliczanie wspolrzednych noraznika, aby pole gry znajdowalo sie na srodku
+        int xCenter = (screenWidth  - gameWidth)/2;
+        int yCenter = (screenHeight  - gameHeight)/2;
+        
+        
+        //tworzenie obiektu klasy GameWindow - konstruktor wywoła dalsze akcje
+        GameWindow gw = new GameWindow(gameWidth, gameHeight, xCenter, yCenter);
+     
+    }
+    
+}
